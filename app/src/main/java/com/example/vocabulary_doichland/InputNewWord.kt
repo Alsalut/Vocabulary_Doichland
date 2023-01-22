b7f2c691b8bbdb1a51ed7568509defdc63cd015d
Alsalut <alsalut@gmail.com>
2023-01-22 22:56:27 +0300

    Work with 3 MutableList

--- app/src/main/java/com/example/vocabulary_doichland/InputNewWord.kt
+++ app/src/main/java/com/example/vocabulary_doichland/InputNewWord.kt
@@ -1,7 +1,6 @@
 package com.example.vocabulary_doichland
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_input_new_word.*
 
@@ -12,17 +11,24 @@ class InputNewWord : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input_new_word)
 
-        // добавляем новый элемент в map
-        btn_save_new_word.setOnClickListener {
-            if (et_new_word.text.toString().isBlank() or et_new_answer.text.toString()
-                    .isBlank()
-            ) return@setOnClickListener
+        val sharedPreferences = getSharedPreferences(keyMemory, MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
 
-            val sharedPreferences = getSharedPreferences(keyMemory, MODE_PRIVATE)
-            val editor = sharedPreferences.edit()
-            Log.d(myTag, "Across arr created")
-            editor.putString(keyWords, "${sharedPreferences.getString(keyWords,"")}$strSplit${et_new_word.text}" )
-            editor.putString(keyAnswers, "${sharedPreferences.getString(keyAnswers,"")}$strSplit${et_new_answer.text}" )
+        // добавляем новый элемент в sharedPreferences
+        btn_save_new_word.setOnClickListener {
+            if (et_new_word.text.toString().isBlank() or et_new_answer.text.toString().isBlank()) return@setOnClickListener
+
+            if (listWords.isNotEmpty())
+            {
+                editor.putString(keyWords, "${sharedPreferences.getString(keyWords, strPlug)}$strSplit${et_new_word.text}")
+                editor.putString(keyAnswers, "${sharedPreferences.getString(keyAnswers, strPlug)}$strSplit${et_new_answer.text}")
+            }
+            else
+            {
+                editor.putString(keyWords, et_new_word.text.toString())
+                editor.putString(keyAnswers, et_new_answer.text.toString())
+            }
+
             editor.apply()
             finish()
         }
--- app/src/main/java/com/example/vocabulary_doichland/MainActivity.kt
+++ app/src/main/java/com/example/vocabulary_doichland/MainActivity.kt
@@ -2,17 +2,19 @@ package com.example.vocabulary_doichland
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 const val keyMemory = "local_memory"
-const val keyWords = "wo1rds"
+const val keyWords = "words"
 const val keyAnswers = "answers"
 const val strSplit = "&split&"
+const val strPlug = "strPlug" // строка-заглушка, если в sharedPreferences ничего нет
 var countWord = 0
-lateinit var mapWords: Map<String, String>
-const val myTag = "myTag"
+val listWords = mutableListOf<String>()
+val listAnswers = mutableListOf<String>()
 
 class MainActivity : AppCompatActivity()
 {
@@ -28,50 +30,74 @@ class MainActivity : AppCompatActivity()
 
         val sharedPreferences = getSharedPreferences(keyMemory, MODE_PRIVATE)
 
+        // очищаем sharedPreferences
+//        val editor = sharedPreferences.edit()
+//        editor.clear()
+//        editor.apply()
+
         // получаем строку из лок. памяти
-        val strWords = sharedPreferences.getString(keyWords, "")
-        val strAnswers = sharedPreferences.getString(keyAnswers, "")
+        val strWords = sharedPreferences.getString(keyWords, strPlug)
+        val strAnswers = sharedPreferences.getString(keyAnswers, strPlug)
 
         // заполняем массивы
         val arrWords = strWords!!.split(strSplit)
         val arrAnswers = strAnswers!!.split(strSplit)
 
-        // заполняем Map
-        mapWords = mapOf<String, String>()
-        var listForShuffleIndexis = mutableListOf<Int>()
-        Log.d(myTag, "Across map and list created")
-        for (index in arrWords.indices)
+        // заполняем списки
+        for (index in 0 until arrWords.size)
         {
-            mapWords = mapWords + Pair(arrWords[index], arrAnswers[index])
-            listForShuffleIndexis.add(index)
+            listWords.add(arrWords[index])
+            listAnswers.add(arrAnswers[index])
         }
 
-        // перемешиваем list
-        listForShuffleIndexis.shuffle()
+        // удаляем строки-заглушки strPlug
+        listWords.remove(strPlug)
+        listAnswers.remove(strPlug)
+
+        // заполняем список-shuffled
+        val listRandomIndex = mutableListOf<Int>()
+
+        if (listWords.size == 0) listRandomIndex.add(0)
+        else for (index in listWords.indices) listRandomIndex.add(index)
+
+        // перемешиваем listRandomIndex
+        listRandomIndex.shuffle()
 
         // выводим первое случайное слово
-        tv_word.text = arrWords[listForShuffleIndexis[countWord]]
+        if (listWords.size == 0)
+        {
+            tv_word.text = getText(R.string.str_no_word)
+            tv_see_answer.visibility = View.INVISIBLE
+            btn_next_word.visibility = View.INVISIBLE
+        }
+        else
+        {
+            tv_word.text = listWords[listRandomIndex[countWord]]
+            tv_see_answer.visibility = View.VISIBLE
+            btn_next_word.visibility = View.VISIBLE
+        }
 
         // при нажатии на tv_see_answer показываем в нём ответ
         tv_see_answer.setOnClickListener {
-            tv_see_answer.text = mapWords[arrWords[listForShuffleIndexis[countWord]]]
+            tv_see_answer.text = listAnswers[listRandomIndex[countWord]]
         }
 
-
-        // при нажатии на кнопку "Следующее слово" выводим по очереди все элементы Map
+        // при нажатии на кнопку "Следующее слово" выводим следующий элемент, и далее по кругу
         btn_next_word.setOnClickListener {
-            countWord = if (countWord == arrWords.size - 1) 0 else countWord + 1
+            countWord = if (countWord >= (listWords.size - 1)) 0 else (countWord + 1)
             tv_see_answer.text = getText(R.string.str_see_answer)
-            tv_word.text = arrWords[listForShuffleIndexis[countWord]]
+            tv_word.text = listWords[listRandomIndex[countWord]]
         }
 
-        // при нажатии на кнопку переходим в InputNewWord
+        // при нажатии на кнопку "Добавить новое слово" переходим в InputNewWord
         btn_add_new_word.setOnClickListener {
             startActivity(Intent(this, InputNewWord::class.java))
         }
     }
 
     // при нажатии "Назад" закрываем приложение и удаляем его из списка программ
+    @Suppress("DEPRECATION")
+    @Deprecated("Deprecated in Java")
     override fun onBackPressed()
     {
         super.onBackPressed()
